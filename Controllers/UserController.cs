f6ab39ef2ba2c13edb8ab1cb262d84334f835116
Matteo Massimello <matteo.massimello@gmail.com>
2026-01-08 09:06:54 +0100

    feat: some add

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -33,10 +33,53 @@ namespace ProgettoProva.web.Controllers
 
         [HttpPost]
         public ActionResult Register(UserViewModel input) {
-            foreach ()
-            {
 
+            char[] specialChar = new char[] { '$', '-', '_', '=' };
+
+            bool upper = false;
+            bool lower = false;
+            bool number = false;
+            bool special = false;
+
+            foreach (var c in input.Password) {
+                if (char.IsUpper(c)) {
+                    upper = true;
+                }
+                else if (char.IsLower(c))
+                {
+                    lower = true;
+                }
+                else if (char.IsDigit(c))
+                {
+                    number = true;
+                }
+                else if (specialChar.Any(sc => c == sc)) {
+                    special = true;
+                }
+
+                if (upper && lower && number && special) 
+                    break;
+                
             }
+
+            if (!upper)
+            {
+                ModelState.AddModelError("Password", "La password deve contenere almeno una maiuscola");
+            }
+            if (!lower)
+            {
+                ModelState.AddModelError("Password", "La password deve contenere almeno una minuscola");
+            }
+            if (!number)
+            {
+                ModelState.AddModelError("Password", "La password deve contenere almeno un numero");
+            }
+            if (!special)
+            {
+                ModelState.AddModelError("Password", "La password deve contenere almeno carattere tra ( "+ string.Join("", specialChar) +" )");
+            }
+
+
             return View(input);
         }
     }
